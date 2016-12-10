923d42193bbe20c21dfa56caf23f2acf8cb2e49c
yithril <yithril@gmail.com>
2016-12-10 11:02:22 +0800

    method stubs

--- datastructures/stack.py
+++ datastructures/stack.py
@@ -1,2 +1,13 @@
 class Stack:
-    pass
+    def __init__(self):
+        pass
+    def push(self, elem):
+        raise NotImplementedError
+    def __len__(self):
+        return 0
+    def empty(self):
+        return len(self)==0
+    def pop(self):
+        raise IndexError
+    def peep(self):
+        raise IndexError
